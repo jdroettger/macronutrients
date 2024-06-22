6d399c50b4a3a7d01881f8b0defcac19cb328e33
jdroettger <96598026+jdroettger@users.noreply.github.com>
2024-06-22 12:54:08 -0500

    Initial Commit

---
+++ src/main/java/net/planetroettger/macronutrients/MacronutrientsApplication.java
@@ -0,0 +1,13 @@
+package net.planetroettger.macronutrients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacronutrientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacronutrientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/planetroettger/macronutrients/MacronutrientsApplicationTests.java
@@ -0,0 +1,9 @@
+package net.planetroettger.macronutrients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacronutrientsApplicationTests {
+
+}
